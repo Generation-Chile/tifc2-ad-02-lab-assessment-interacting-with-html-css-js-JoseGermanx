811fd267bf461b196e2776c705568f907ac0275a
Jose Germán Martínez <jgermanmm@gmail.com>
2024-04-25 10:27:58 -0400

    Actualizar página principal con nuevos elementos y estilos

---
+++ script.js
@@ -0,0 +1,11 @@
+const miTitulo = document.getElementById('miTitulo');
+
+miTitulo.innerText = "Adios";
+miTitulo.style.color = "orange";
+
+
+function cambiarColor() {
+  
+  var miEncabezado = document.getElementById('cambiarColor');
+  miEncabezado.style.color = "brown";
+}
